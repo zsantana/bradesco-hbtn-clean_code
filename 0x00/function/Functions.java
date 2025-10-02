07ab3f1fa61f99928950a966e8bf4335572bd0d4
zsantana <reinaldojsantana@gmail.com>
2025-10-02 13:50:30 +0000

    Clean code

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -1,15 +1,20 @@
 public class Functions {
+
+    // Repositórios e serviços devem ser declarados aqui (Assumindo injeção via construtor ou @Autowired)
     private final IncomeRepository inRepo;
     private final ExpenseRepository outRepo;
     private final UserService userService;
+    private final UserRepository userRepository;
     private final VersionService versionService;
     private final EnvironmentService environmentService;
     private final ReleaseService releaseService;
 
+    // Construtor assumido para injecção dos dependentes
     public Functions(
         IncomeRepository inRepo,
         ExpenseRepository outRepo,
         UserService userService,
+        UserRepository userRepository,
         VersionService versionService,
         EnvironmentService environmentService,
         ReleaseService releaseService
@@ -17,34 +22,49 @@ public class Functions {
         this.inRepo = inRepo;
         this.outRepo = outRepo;
         this.userService = userService;
+        this.userRepository = userRepository;
         this.versionService = versionService;
         this.environmentService = environmentService;
         this.releaseService = releaseService;
     }
 
-    public void saveIncome(Income income) {
+    // Cada método deve fazer somente uma coisa e ter argumentos mínimos
+
+    public void saveIncome(Income income){
         inRepo.save(income);
     }
 
-    public void saveExpense(Expense expense) {
+    public void saveExpense(Expense expense){
         outRepo.save(expense);
     }
 
-    // REST endpoint: único método correto para salvar usuário
     @PostMapping("/user")
-    public User saveUser(User user) {
+    public User saveUser(User user){
         return userService.save(user);
     }
 
-    public Environment getEnvironment() {
+    // Argumentos mínimos e nome claro
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    // Sem efeitos colaterais, lógica separada
+    public User saveUserNoSideEffects(User user){
+        return userRepository.save(user);
+    }
+
+    public void setRole(User user){
+        user.setRole(user);     
+    }
+
+    // Evite duplicação: extraia para um método privado
+    public Environment getEnvironment(){
         return environmentService.getEnvironment(getVersion());
     }
-
-    public Release getRelease() {
+    public Release getRelease(){
         return releaseService.getRelease(getVersion());
     }
-
-    private String getVersion() {
+    private String getVersion(){ 
         return versionService.getVersion();
     }
 }
